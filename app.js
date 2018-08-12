290649eae496039ff0b83ec9e92da539dcd3afce
dhruvimak <dhruvipmakavana@yahoo.com>
2018-08-13 00:11:15 +0530

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.sendFile('/index.html'))
