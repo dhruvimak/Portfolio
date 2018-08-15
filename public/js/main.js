40c9913c3a8fe016b2bc63dd0e23582701858ef4
dhruvimak <dhruvipmakavana@yahoo.com>
2018-08-15 23:14:58 +0530

    navbar

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 let showMenu = false;
@@ -15,7 +14,6 @@ function toggleMenu(){
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
         navItems.forEach(item=>item.classList.add('show'));
         showMenu=!showMenu;
 
@@ -24,7 +22,6 @@ function toggleMenu(){
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
         navItems.forEach(item=>item.classList.remove('show'));
         showMenu=!showMenu;
     }
