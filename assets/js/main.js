21f374dfe9c5e68e8104658e0a8a977fcb7327a7
dhruvimak <dhruvipmakavana@yahoo.com>
2021-05-15 20:08:14 +0530

    business card

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,27 @@ let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu(){
+function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        navItems.forEach(item=>item.classList.add('show'));
-        showMenu=!showMenu;
+        navItems.forEach(item => item.classList.add('show'));
+        showMenu = !showMenu;
 
     } else {
 
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        navItems.forEach(item=>item.classList.remove('show'));
-        showMenu=!showMenu;
+        navItems.forEach(item => item.classList.remove('show'));
+        showMenu = !showMenu;
     }
 }
+
+window.addEventListener('load', (event) => {
+    var date = new Date();
+    var currentYear = date.getFullYear();
+    var dateEle = document.getElementById('currentYear');
+    dateEle.textContent = currentYear;
+});
